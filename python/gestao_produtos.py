c66fbd98affba598474639898d5880134bdc8cad
Sónia Rodrigues <soniaalexandraa94@gmail.com>
2023-02-08 17:58:50 +0000

    feat: add method str and iter in class CatalogoProdutos

--- python/gestao_produtos.py
+++ python/gestao_produtos.py
@@ -111,16 +111,28 @@ class CatalogoProdutos:
         return self.prods.get(id)
     #:
 
-    def pesquisa(self, criterio):
+    def pesquisa(self, criterio) -> 'CatalogoProdutos':
         encontrados = CatalogoProdutos()
-        for prod in self._prods:
+        for prod in self._prods.values():
             if criterio(prod):
                 encontrados.append(prod)
         return encontrados
     #:
 
+    def __str__(self):
+        class_name = self.__class__.__name__
+        return f'{class_name}[#produtos = {len(self._prods)}]'
+    #:
+
+    def __iter__(self):
+        for prod in self._prods.values():
+            yield prod
+        #:
+    #:
+
     def __len__(self):
         return len(self._prods)
+    #:
 #:
 
 
@@ -134,6 +146,7 @@ def main() -> None:
     produtos.append(Produto(30987, 'pão de milho', 'AL', 2, dec('1')))
     produtos.append(Produto(30098, 'leite mimosa', 'AL', 10, dec('2')))
     produtos.append(Produto(21109, 'fairy', 'DL', 20, dec('3')))
+    # produtos.append(Produto(21109, 'fairy', 'DL', 20, dec('3')))
 
     produtos._dump()
 #:
