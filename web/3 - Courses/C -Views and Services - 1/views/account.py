c8563f17c0bdfa0e72428b916426d5d757c03f92
Sónia Rodrigues <soniaalexandraa94@gmail.com>
2023-03-06 22:14:17 +0000

    add some files U part1

---
+++ web/3 - Courses/C -Views and Services - 1/views/account.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter
+from fastapi_chameleon import template
+
+router = APIRouter()
+
+
+@router.get('/account')
+async def index():
+    return {}
+
+
+@router.get('/account/register')
+async def register():
+    return {
+        'error': False,
+        'error_msg': 'There was an error with your data. Please try again.'
+    }
+
+
+@router.get('/account/login')
+async def login():
+    return {
+        'error': False,
+        'error_msg': 'There was an error with your data. Please try again.'
+    }
